504b5674adcdd2066bc3290d254cfaee163f3b7f
Sudeb Sarkar <sudebsarkar21@gmail.com>
2025-07-14 01:31:36 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mysql = require('mysql'); 
+const cors = require('cors');
 
-app.get("/api",(req,res) => {
-    res.json({
-        "users": ["userone", "usertwo", "userthree"],
-    })
+const app = express();
+app.use(cors());
+
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "todolist"
+}); 
+
+app.get('/api', (req,res) => {
+    return res.json({users: ["user1", "user2", "user3"]});
+})
+
+app.get('/users', (req,res) => {
+  const sql = "Select * from Users" ;
+  db.query(sql,(err,data) => {
+    if (err) { return res.json(err); }
+    return res.json(data);
+  })
 })
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000")
-})
+    console.log("Server is running on port 5000");
+});
